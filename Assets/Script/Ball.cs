968cd1e8684ee1fef1885ba018ff1fd3d0350c7d
username <2285404212@qq.com>
2022-09-01 22:12:09 +0800

    first commit

---
+++ Assets/Script/Ball.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Ball : MonoBehaviour, IDragHandler
+{
+     
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+
+    //拖拽
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 mouseScreenPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPos.z);
+        transform.position = Camera.main.ScreenToWorldPoint(mouseScreenPos);
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ball")
+        {
+            GameManager.Instance.score += 0.25f;
+            GameManager.Instance.count -= 0.25f;
+            Destroy(transform.gameObject);
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public float score;
+    public int input;
+    //private bool isLock;
+    private Vector3 firstPos;
+    public float count;
+    public GameObject prefab;
+    public Transform  button;
+    
+
+    public Transform  text;
+    GUIStyle style = new GUIStyle();
+
+    public static GameManager Instance = null;
+
+    private void Awake()
+    {
+        
+        Instance = this;
+        score = 0;
+    }
+    void Start()
+    {
+        
+        style.fontSize = 40;
+        style.fontStyle = FontStyle.Bold;
+        //isLock  = true;
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(0, 0, 200, 100), "当前分：" + score, style);
+        GUI.Label(new Rect(0, 50, 200, 100), "剩余球：" + count,style);
+        //if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 50), "开始游戏") && isLock)
+        //{
+        //}
+        if (score != 0 && score == input)
+        {
+            GUI.Label(new Rect(400, 400, 400, 200), "游戏胜利！", style);
+        }
+        if (GUI.Button(new Rect(800, 0, 200, 50), "重新开始"))
+            {
+            button.gameObject.SetActive(true);
+                score = 0;
+                //isLock = true;
+                text.gameObject.SetActive(true);
+                SceneManager.LoadScene("SampleScene");
+            }
+    }
+    
+
+    public void ButtonStartGame()
+    {
+        //isLock = false;
+        button.gameObject.SetActive(false);
+        input = int.Parse(text.GetComponent<InputField>().text);
+        count = input;
+        for (int i = 0; i < input; i++)
+        {
+            int ran1 = Random.Range(-4, 4);
+            int ran2 = Random.Range(-4, 4);
+            firstPos = new Vector3(ran1, 0.25f, ran2);
+            GameObject ball = Instantiate(prefab.gameObject);
+            ball.transform.position = firstPos;
+            text.gameObject.SetActive(false);
+        }
+        
+    }
+}
